d724d0f985c158288820b6c6c6625cbf3e8394d7
kash / Hytus <kash@hytus.net>
2021-08-18 16:07:26 +0900

    :zap: implement markdown converter

--- services/renderer-go/renderer/renderer.go
+++ services/renderer-go/renderer/renderer.go
@@ -3,23 +3,34 @@ package renderer
 import (
 	"bytes"
 	"context"
-	"html/template"
-	"regexp"
+	"errors"
+
+	"github.com/yuin/goldmark"
 )
 
-var urlRE = regexp.MustCompile(`https?://[^\s]+`)
-var linkTmpl = template.Must(template.New("link").Parse(`<a href="{{.}}">{{.}}</a>`))
+// var urlRE = regexp.MustCompile(`https?://[^\s]+`)
+// var linkTmpl = template.Must(template.New("link").Parse(`<a href="{{.}}">{{.}}</a>`))
 
 // Render は受け取った文書を HTML に変換する
 func Render(ctx context.Context, src string) (string, error) {
 	// TODO: これはサンプル実装 (URL の自動リンク) です
-	html := urlRE.ReplaceAllStringFunc(src, func(url string) string {
-		var w bytes.Buffer
-		err := linkTmpl.ExecuteTemplate(&w, "link", url)
-		if err != nil {
-			return url
-		}
-		return w.String()
-	})
+	// html := urlRE.ReplaceAllStringFunc(src, func(url string) string {
+	// 	var w bytes.Buffer
+	// 	err := linkTmpl.ExecuteTemplate(&w, "link", url)
+	// 	if err != nil {
+	// 		return url
+	// 	}
+
+	// 	return w.String()
+	// })
+
+	// Convert Markdown
+	var w bytes.Buffer
+	if err := goldmark.Convert([]byte(src), &w); err != nil {
+		return "", errors.New("error")
+	}
+
+	html := w.String()
+
 	return html, nil
 }
