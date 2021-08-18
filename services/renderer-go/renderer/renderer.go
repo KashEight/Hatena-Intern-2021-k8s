5fa92dd15090dc19b9dafa41ca6f6f44225e32ad
kash / Hytus <kash@hytus.net>
2021-08-18 18:55:49 +0900

    :construction: force replace src (not working) :fire: remove old code

--- services/renderer-go/renderer/renderer.go
+++ services/renderer-go/renderer/renderer.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"context"
 	"errors"
+	"fmt"
+	"strings"
 
 	"github.com/yuin/goldmark"
 )
@@ -11,20 +13,20 @@ import (
 // var urlRE = regexp.MustCompile(`https?://[^\s]+`)
 // var linkTmpl = template.Must(template.New("link").Parse(`<a href="{{.}}">{{.}}</a>`))
 
+var counter = 0
+
 // Render は受け取った文書を HTML に変換する
 func Render(ctx context.Context, src string) (string, error) {
-	// TODO: これはサンプル実装 (URL の自動リンク) です
-	// html := urlRE.ReplaceAllStringFunc(src, func(url string) string {
-	// 	var w bytes.Buffer
-	// 	err := linkTmpl.ExecuteTemplate(&w, "link", url)
-	// 	if err != nil {
-	// 		return url
-	// 	}
+	// Increment counter and replace
 
-	// 	return w.String()
-	// })
+	counter++
+
+	if strings.Contains(src, "<+>") {
+		_ = strings.Replace(src, "<+>", fmt.Sprintf("%07d", counter), -1)
+	}
 
 	// Convert Markdown
+
 	var w bytes.Buffer
 	if err := goldmark.Convert([]byte(src), &w); err != nil {
 		return "", errors.New("error")
